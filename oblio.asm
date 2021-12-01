2732d310011fa9aad4805bd8e3c4e86234abf23f
arinotsorry <aew1756@rit.edu>
2021-12-01 14:41:36 -0500

    Have loop to generate targets
    RIP I've lost this stupid loop twice after debugging and having to force quit MARS (my MIPS IDE). Something's funky with how MARS saves files on macs, I learned the hard way

--- oblio.asm
+++ oblio.asm
@@ -16,7 +16,19 @@
 .text
 	j main
 	
+# ------------------
+# CHALLENGER METHODS
+# ------------------	
+	
 	challenger:
+		# Print that we're in the challenger function
+		li $v0, 4
+		la $a0, challenger_method
+		syscall
+		
+		# call generate_targets
+		jal generate_targets
+		
 		# Print that we're in the challenger function
 		li $v0, 4
 		la $a0, challenger_method
@@ -25,6 +37,10 @@
 		# end
 		j end
 		
+# ---------------
+# GUESSER METHODS
+# ---------------
+
 	guesser:
 		# Print that we're in the guesser function
 		li $v0, 4
@@ -33,6 +49,120 @@
 		
 		# end
 		j end
+		
+# --------------
+# TARGET METHODS
+# --------------
+
+	generate_targets:
+		# $t0 is a, $t1 is b, $t2 is c, $t3 is d
+		# $t4 is counter
+		# $t5 is approved number
+		move $t0, $zero
+		move $t1, $zero
+		move $t2, $zero
+		move $t3, $zero
+		move $t4, $zero
+		
+		# stack shenanigans
+		addi $sp, $sp, -8 	# space for 1 register
+		sw $ra, 0($sp)
+		
+		jal loop_a
+		
+		# Print total number
+		li $t4, 500
+		li $v0, 1
+		move $a0, $t4
+		syscall
+		
+		# stack shenanigans 2: Electric Boogaloo
+		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+		jr $ra
+	
+		
+	loop_a:
+		# stack shenanigans
+		addi $sp, $sp, -8 	# space for 1 register
+		sw $ra, 0($sp)
+		
+		# call loop_b
+		move $t1, $zero
+		jal loop_b
+		
+		# stack shenanigans 2: Electric Boogaloo
+		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+		
+		# check conditional
+		addi $t0, $t0, 1		# i++
+		slti $t6, $t0, 10		# $t6 = 1 if i < 10
+		bne $t6, $zero, loop_a
+		
+		jr $ra
+		
+	loop_b:
+		# stack shenanigans
+		addi $sp, $sp, -8 	# space for 1 register
+		sw $ra, 0($sp)
+		
+		# call loop_c
+		move $t2, $zero
+		jal loop_c
+		
+		# stack shenanigans 2: Electric Boogaloo
+		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+		
+		# check conditional
+		addi $t1, $t1, 1		# i++
+		slti $t6, $t1, 10		# $t6 = 1 if i < 10
+		bne $t6, $zero, loop_b
+		
+		jr $ra
+
+	loop_c:
+		# stack shenanigans
+		addi $sp, $sp, -8 	# space for 1 register
+		sw $ra, 0($sp)
+		
+		# call loop_d
+		move $t3, $zero
+		jal loop_d
+		
+		# stack shenanigans 2: Electric Boogaloo
+		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+		
+		# check conditional
+		addi $t2, $t2, 1		# i++
+		slti $t6, $t2, 10		# $t6 = 1 if i < 10
+		bne $t6, $zero, loop_c
+		
+		jr $ra
+
+	loop_d:
+		# stack shenanigans
+		addi $sp, $sp, -8 	# space for 1 register
+		sw $ra, 0($sp)
+		
+		# call check_number
+		
+		# stack shenanigans 2: Electric Boogaloo
+		lw $ra, 0($sp)
+		addi $sp, $sp, 8
+		
+		# check conditional
+		addi $t3, $t3, 1		# i++
+		slti $t6, $t3, 10		# $t6 = 1 if i < 10
+		bne $t6, $zero, loop_d
+		
+		jr $ra
+		
+# ----
+# MAIN
+# ----
 	
 	end:
 		# Tell system this is end of main function
